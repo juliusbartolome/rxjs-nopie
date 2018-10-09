779a383940a2ac09f8538baf9dcc3b5da6402abd
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2018-10-09 21:41:40 +0800

    added error handling with retry logic using rxjs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { fromEvent, Observable } from "rxjs";
-import { map, delay } from "rxjs/operators";
+import { flatMap, delay, scan, retryWhen, takeWhile } from "rxjs/operators";
 
 let output = document.getElementById("output");
 let button = document.getElementById("button");
@@ -7,25 +7,47 @@ let button = document.getElementById("button");
 let click = fromEvent(button, "click");
 
 function load(url: string) {
-    let xhr = new XMLHttpRequest();
-
-    xhr.addEventListener("load", () => {
-        let posts = JSON.parse(xhr.responseText);
-        posts.forEach(post => {
-            let div = document.createElement("div");
-            let hr = document.createElement("hr");
-            div.innerText = post.title;
-            output.appendChild(div);
-            output.appendChild(hr);
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();
+        xhr.addEventListener("load", () => {
+            if (xhr.status == 200) {
+                observer.next(JSON.parse(xhr.responseText))
+            } else {
+                observer.error(xhr.status);
+            }
         });
-    });
-
-    xhr.open("GET", url);
-    xhr.send();
+        xhr.open("GET", url);
+        xhr.send();
+    }).pipe(
+        retryWhen(retryStrategy({attempts: 5, delayValue: 1500}))
+    );
 }
 
-click.subscribe(
-    value => load("https://jsonplaceholder.typicode.com/posts"),
-    e => console.error(`error: ${e}`),
-    () => console.log("complete")
+function retryStrategy({attempts = 3, delayValue = 10}) {
+    return function(errors) {
+        return errors.pipe(
+            scan(acc => acc + 1, 0),
+            takeWhile(acc => acc < attempts),
+            delay(delayValue)
+        );
+    }
+}
+
+function renderPosts(posts) {
+    posts.forEach(post => {
+        let div = document.createElement("div");
+        let hr = document.createElement("hr");
+        div.innerText = post.title;
+        output.appendChild(div);
+        output.appendChild(hr);
+    });
+}
+
+
+click.pipe(
+    flatMap(e => load("https://jsonplaceholder.typicode.com/posts2"))
+).subscribe(
+  renderPosts,
+  e => console.error(`error: ${e}`),
+  () => console.log("complete")
 );
