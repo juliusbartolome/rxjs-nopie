c280fb26595afc41a7a88e81a051825993cdd88d
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2018-10-12 14:54:48 +0800

    added error handling with retry logic

--- src/dataLoader.ts
+++ src/dataLoader.ts
@@ -1,5 +1,5 @@
 import { from, Observable, defer } from "rxjs";
-import { delay, scan, retryWhen, takeWhile } from "rxjs/operators";
+import { delay, scan, retryWhen } from "rxjs/operators";
 
 export function load(url: string) {
     return Observable.create(observer => {
@@ -11,6 +11,7 @@ export function load(url: string) {
                 observer.error(xhr.status);
             }
         });
+
         xhr.open("GET", url);
         xhr.send();
     }).pipe(
@@ -20,16 +21,34 @@ export function load(url: string) {
 
 export function loadWithFetch(url: string) {
     return defer(() => {
-        let promise = fetch(url).then(r => r.json());
+        let promise = fetch(url)
+                        .then(response => {
+                            if (response.status === 200) {
+                                return response.json();
+                            } else {
+                                return Promise.reject(response);
+                            }
+                        });
+        
         return from(promise);
-    });
+    }).pipe(
+        retryWhen(retryStrategy())
+    );
 }
 
-function retryStrategy({ attempts = 3, delayValue = 10 }) {
+function retryStrategy({ attempts = 4, delayValue = 10 } = {}) {
     return function (errors) {
         return errors.pipe(
-            scan(acc => acc + 1, 0),
-            takeWhile(acc => acc < attempts),
+            scan(acc => {
+                console.log(errors);
+                
+                acc += 1;
+                if (acc < attempts) {
+                    return acc;
+                } else {
+                    throw new Error(errors);
+                }
+            }, 0),
             delay(delayValue)
         );
     }
--- src/index.ts
+++ src/index.ts
@@ -2,54 +2,61 @@ import { fromEvent, from, of, merge, Observable, throwError } from "rxjs";
 import { flatMap, catchError } from "rxjs/operators";
 import { loadWithFetch } from "./dataLoader";
 
-let source = merge(
-    of(1),
-    from([2, 3, 4]),
-    throwError(new Error("Stop!")),
-    of(5)
-);
+// let source = merge(
+//     of(1),
+//     from([2, 3, 4]),
+//     throwError(new Error("Stop!")),
+//     of(5)
+// );
 
-// let source = Observable.create(observer => {
-//     observer.next(1);
-//     observer.next(2);
-//     observer.error("Stop!");
-//     throw new Error("Stop! Error thrown!");
-//     observer.next(3);
-//     observer.complete();
-// });
+// // let source = Observable.create(observer => {
+// //     observer.next(1);
+// //     observer.next(2);
+// //     observer.error("Stop!");
+// //     throw new Error("Stop! Error thrown!");
+// //     observer.next(3);
+// //     observer.complete();
+// // });
 
-source = source.pipe(
-    catchError(error => {
-        console.error(`caught: ${error}`);
-        return of(10);
-    })
-);
+// source = source.pipe(
+//     catchError(error => {
+//         console.error(`caught: ${error}`);
+//         return of(10);
+//     })
+// );
 
-source.subscribe(
-    value => console.log(`value: ${value}`),
-    error => console.error(`error: ${error}`),
-    () => console.log("complete")
-);
-
-// let output = document.getElementById("output");
-// let button = document.getElementById("button");
-
-// let click = fromEvent(button, "click");
-
-// function renderPosts(posts) {
-//     posts.forEach(post => {
-//         let div = document.createElement("div");
-//         let hr = document.createElement("hr");
-//         div.innerText = post.title;
-//         output.appendChild(div);
-//         output.appendChild(hr);
-//     });
-// }
-
-// click.pipe(
-//     flatMap(e => loadWithFetch("https://jsonplaceholder.typicode.com/posts"))
-// ).subscribe(
-//     renderPosts,
-//     e => console.error(`error: ${e}`),
+// source.subscribe(
+//     value => console.log(`value: ${value}`),
+//     error => console.error(`error: ${error}`),
 //     () => console.log("complete")
 // );
+
+let output = document.getElementById("output");
+let button = document.getElementById("button");
+
+let click = fromEvent(button, "click");
+
+function renderPosts(posts) {
+    posts.forEach(post => {
+        let div = document.createElement("div");
+        let hr = document.createElement("hr");
+        div.innerText = post.title;
+        output.appendChild(div);
+        output.appendChild(hr);
+    });
+}
+
+loadWithFetch("https://jsonplaceholder.typicode.com/postss")
+    .subscribe(
+        renderPosts,
+        e => console.log(`error: ${e}`),
+        () => console.log("complete")
+    );
+
+click.pipe(
+    flatMap(e => loadWithFetch("https://jsonplaceholder.typicode.com/posts"))
+).subscribe(
+    renderPosts,
+    e => console.error(`error: ${e}`),
+    () => console.log("complete")
+);
